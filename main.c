d5d57ace75cf1a3c58b9414ba274651928ec8c5a
naurafarah-sketch <naurafarah@student.upi.edu>
2025-12-12 16:29:51 +0700

    naura

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
 void statusBuku(){
-    printf("Hello Anjay");
+    #include <stdio.h>
+    #include <string.h>
+
+    struct Buku {
+        char kode[20];
+        char judul[50];
+        char kelas[10];
+        char mapel[20];
+    };
+    struct Buku database[200];
+    int jumlahBuku = 0;
+
+    void tampilBuku() {
+        printf("Daftar Buku:\n");
+        for (int i = 0; i < jumlahBuku; i++) {
+            printf("Kode: %s, Judul: %s, Kelas: %s, Mapel: %s\n",
+                   database[i].kode,
+                   database[i].judul,
+                   database[i].kelas,
+                   database[i].mapel);
+        }
+    }
+    
+    {
+        /* data */
+    };
+    
 }
 int main(){
     statusBuku();
