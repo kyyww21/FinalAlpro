a28e7847a798b93684d4c97c53a5ba652a04891c
naurafarah-sketch <naurafarah@student.upi.edu>
2025-12-12 17:05:22 +0700

    naura

--- main.c
+++ main.c
@@ -7,23 +7,50 @@ void statusBuku(){
     struct Buku {
         char kode[20];
         char judul[50];
-        char kelas[10];
-        char mapel[20];
-    };
+        
     struct Buku database[200];
-    int jumlahBuku = 0;
+    int totalBuku = 0;
 
-    void tampilBuku() {
+    void tambahBuku() {
         printf("Daftar Buku:\n");
+
+        if (totalBuku == 0) {
+            printf("Tidak ada buku dalam database.\n");
+
+            return;
+        }
+
         for (int i = 0; i < jumlahBuku; i++) {
             printf("Kode: %s, Judul: %s, Kelas: %s, Mapel: %s\n",
                    database[i].kode,
-                   database[i].judul,
+                   database[i].judul
                    database[i].kelas,
-                   database[i].mapel);
+                   database[i].mapel;
+            );
         }
     }
-    
+    void tambahBuku(){
+        printf(" \n=== Tambah Buku ===\n");
+        printf("Masukkan kode buku: ");
+        scanf("%s", database[jumlahBuku].kode);
+
+        printf("Masukkan judul buku: ");
+        scanf("%s", database[jumlahBuku].judul);
+
+        printf("Masukkan kelas: ");
+        scanf("%s", database[jumlahBuku].kelas);
+
+        printf("Masukan mapel: ");
+        scanf("%s", database[jumlahBuku].mapel);
+
+        jumlahBuku++;
+        printf("Buku berhasil ditambahkan!\n");
+
+
+        
+
+    }
+
     {
         /* data */
     };
