0bc94369050841e0ddef1d9b854eb284f125a9f9
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-12 17:11:34 +0700

    Coba-Sorting

--- main.c
+++ main.c
@@ -30,3 +30,16 @@ void penambahanBuku() {
     printf("\n-- Buku berhasil ditambahkan --\n");
 }
 
+void bubbleSortBuku() {
+    struct Buku temp;
+    
+    for (int i = 0; i < totalBuku - 1; i++) {
+        for (int j = 0; j < totalBuku - i - 1; j++) {
+            if (strcmp(perpustakaan[j].Judul, perpustakaan[j+1].Judul) > 0) {
+                temp = perpustakaan[j];
+                perpustakaan[j] = perpustakaan[j+1];
+                perpustakaan[j+1] = temp;
+            }
+        }
+    }
+}
