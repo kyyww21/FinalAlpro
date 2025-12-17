65bf89d185e878340da1a421bd2c6a797d51fc9b
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-17 15:03:44 +0700

    coba

--- main.c
+++ main.c
@@ -9,7 +9,6 @@ struct Buku {
 };
 struct Peminjaman {
     char NamaPeminjam[50];
-    char Kelas[10];
     char KodeBuku[20];
     char JudulBuku[50];
     int JumlahPinjam;
@@ -52,68 +51,6 @@ void bubbleSortBuku() {
                 perpustakaan[j+1] = temp;
             }
         }
-    }
-}
-
-
-void peminjamanBuku() {
-    char kode[20];
-    char nama[50];
-    char kelas[20];
-    int ditemukan = 0;
-    int jumlahPinjam;
-    
-    printf("\n--- Peminjaman Buku ---\n");
-    
-    printf("Masukkan Nama Peminjam : ");
-    scanf(" %[^\n]", nama);
-    
-    printf("Masukkan Kelas         : ");
-    scanf(" %[^\n]", kelas);
-    
-    printf("Masukkan Kode Buku     : ");
-    scanf(" %[^\n]", kode);
-    for (int i = 0; i < totalBuku; i++) {
-        if (strcmp(perpustakaan[i].Kode, kode) == 0) {
-            ditemukan = 1;
-            
-            printf("\n-- Buku Ditemukan --\n");
-            printf("Judul Buku    : %s\n", perpustakaan[i].Judul);
-            printf("Kode Buku     : %s\n", perpustakaan[i].Kode);
-            
-            if (perpustakaan[i].Jumlah > 0) {
-                printf("\nMasukkan Jumlah yang Ingin Dipinjam : ");
-                scanf("%d", &jumlahPinjam);
-                
-                if (jumlahPinjam <= 0) {
-                    printf("\n-- Jumlah tidak valid! --\n");
-                } else if (jumlahPinjam > perpustakaan[i].Jumlah) {
-                    printf("\n-- Stok tidak mencukupi! --\n");
-                    printf("Stok tersedia hanya %d buku\n", perpustakaan[i].Jumlah);
-                } else {
-                    strcpy(daftarPeminjaman[totalPeminjaman].NamaPeminjam, nama);
-                    strcpy(daftarPeminjaman[totalPeminjaman].Kelas, kelas);
-                    strcpy(daftarPeminjaman[totalPeminjaman].KodeBuku, kode);
-                    strcpy(daftarPeminjaman[totalPeminjaman].JudulBuku, perpustakaan[i].Judul);
-                    daftarPeminjaman[totalPeminjaman].JumlahPinjam = jumlahPinjam;
-                    totalPeminjaman++;
-                    
-                    perpustakaan[i].Jumlah -= jumlahPinjam;
-                    
-                    printf("\n-- Peminjaman Berhasil! --\n");
-                    printf("Nama Peminjam : %s\n", nama);
-                    printf("Kelas / Guru  : %s\n", kelas);
-                    printf("Nama Buku     : %s\n", perpustakaan[i].Judul);
-                    printf("Kode Buku     : %s\n", kode);
-                    printf("Jumlah Pinjam : %d\n", jumlahPinjam);
-                }
-            }
-            break;
-        }
-    }
-    
-    if (!ditemukan) {
-        printf("\n-- Buku dengan kode '%s' tidak ditemukan --\n", kode);
     }
 }
 
