49f224a03b7183cec46d08dedcf75100f6e0f03e
Khairul-arch <khairulabdurrahim98@student.upi.edu>
2025-12-17 13:03:16 +0700

    MenuUtama.2

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void menu();
+
 void selesai() {
     printf("Oke, Terimakasih ;)");
 }
@@ -8,86 +10,89 @@ void selesaiPerubahan() {
     printf("Perubahan Telah Disimpan, Terimakasih ;)\n");
 }
 
+void subMenu1() {
+    int subOpsi1;
+    printf("\n===OPSI===\n1. Penambahan Buku\n2. Penghapusan Buku\n3. Status Buku\n4. Daftar Buku\n5. Kembali\n6. Selesai\nMasukkan Opsi Anda : ");
+    scanf("%d", &subOpsi1);
+
+        switch(subOpsi1) {
+            case 1:
+                selesaiPerubahan();
+                subMenu1();
+                break;
+            case 2:
+                selesaiPerubahan();
+                subMenu1();
+                break;
+            case 3:
+                selesaiPerubahan();
+                subMenu1();
+                break;
+            case 4:
+                selesaiPerubahan();
+                subMenu1();
+                break;
+            case 5:
+                menu();
+                break;
+            case 6:
+                selesai();
+                break;
+            default:
+                printf("\nTidak ada Opsi %d di dalam daftar Opsi, Silahkan Pilih Kembali");
+                subMenu1();
+                break;
+        }
+}
+
+void subMenu2() {
+    int subOpsi2;
+    printf("\n===OPSI===\n1. Peminjaman Buku2. Pengembalian Buku\n3. Kembali\n4. Selesai\nMasukkan Opsi Anda : ");
+    scanf("%d", &subOpsi2);
+
+        switch(subOpsi2) {
+            case 1:
+                selesaiPerubahan();
+                subMenu2();
+                break;
+            case 2:
+                selesaiPerubahan();
+                subMenu2();
+                break;
+            case 3:
+                menu();
+                break;
+            case 4:
+                selesai();
+                break;
+            default:
+                printf("\nTidak ada Opsi %d di dalam daftar Opsi, Silahkan Pilih Kembali");
+                subMenu2();
+                break;
+        }
+}
+
 void menu() {
-    int opsi, subOpsi1, subOpsi2;
+    int opsi;
 
     printf("\n===OPSI===\n1. Operator\n2. Pengunjung\n3. Selesai\nMasukkan Opsi Anda : ");
     scanf("%d", &opsi);
 
     switch(opsi) {
         case 1:
-            void subMenu() {
-                printf("\n===OPSI===\n1. Penambahan Buku2. Penghapusan Buku\n3. Status Buku\n4. Daftar Buku\n5. Kembali\n6. Selesai\nMasukkan Opsi Anda : ");
-                scanf("%d", &subOpsi1);
-
-                switch(subOpsi1) {
-                    case 1:
-                        selesaiPerubahan();
-                        menu();
-                        break;
-                    case 2:
-                        selesaiPerubahan();
-                        menu();
-                        break;
-                    case 3:
-                        selesaiPerubahan();
-                        menu();
-                        break;
-                    case 4:
-                        selesaiPerubahan();
-                        menu();
-                        break;
-                    case 5:
-                        menu();
-                        break;
-                    case 6:
-                        selesai();
-                        break;
-                    default:
-                        printf("\nTidak ada Opsi %d di dalam daftar Opsi, Silahkan Pilih Kembali");
-                        subMenu1();
-                        break;
-                }
-            }
             subMenu1();
             break;
         case 2:
-            void subMenu2() {
-                printf("\n===OPSI===\n1. Peminjaman Buku2. Pengembalian Buku\n3. Kembali\n4. Selesai\nMasukkan Opsi Anda : ");
-                scanf("%d", &subOpsi2);
-
-                switch(subOpsi2) {
-                    case 1:
-                        selesaiPerubahan();
-                        menu();
-                        break;
-                    case 2:
-                        selesaiPerubahan();
-                        menu();
-                        break;
-                    case 3:
-                        menu();
-                        break;
-                    case 4:
-                        selesai();
-                        break;
-                    default:
-                        printf("\nTidak ada Opsi %d di dalam daftar Opsi, Silahkan Pilih Kembali");
-                        subMenu2();
-                        break;
-                }
-            }
             subMenu2();
             break;
         case 3:
             selesai();
-            break
+            break;
         default:
             printf("\nTidak ada Opsi %d di dalam daftar Opsi, Silahkan Pilih Kembali");
             subMenu1();
             break;
     }
-    return;
 }
 
 int main(){
