0c833d604238443db168314a4faab002a61d512c
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-17 13:56:29 +0700

    penambahan-listbuku

--- main.c
+++ main.c
@@ -1,19 +1,3 @@
-void daftarBuku() {
-    if (totalBuku == 0) {
-        printf("\n-- Belum ada buku di perpustakaan --\n");
-    }
-    printf("\n=== DAFTAR SEMUA BUKU ===\n");
-    printf("%-30s %-15s %-8s %-10s %-10s\n", "Judul", "Kode", "Total", "Dipinjam", "Tersedia");  // Printf -> printf
-    printf("---------------------------------------------------------------\n");
-
-    for (int i = 0; i < totalBuku; i++) {
-        int tersedia = perpustakaan[i].JumlahTotal - perpustakaan[i].JumlahDipinjam;  // Perpustakaan -> perpustakaan, tambah ;
-        printf("%-30s %-15s %-8d %-10d %-10d\n",  // Tambah tanda kutip "
-               perpustakaan[i].Judul,              // Perpustakaan -> perpustakaan
-               perpustakaan[i].Kode,               // Perpustakaan -> perpustakaan
-               perpustakaan[i].JumlahTotal,        // Perpustakaan -> perpustakaan
-               perpustakaan[i].JumlahDipinjam,     // daftarPeminjaman -> perpustakaan
-               tersedia);
 #include <stdio.h>
 #include <string.h>
 
@@ -68,6 +52,27 @@ void bubbleSortBuku() {
             }
         }
     }
+}
+
+void daftarBuku() {
+    if (totalBuku == 0) {
+        printf("\n-- Belum ada buku di perpustakaan --\n");
+    }
+    printf("\n=== DAFTAR SEMUA BUKU ===\n");
+    printf("%-30s %-15s %-8s %-10s %-10s\n", "Judul", "Kode", "Total", "Dipinjam", "Tersedia");  // Printf -> printf
+    printf("---------------------------------------------------------------\n");
+
+    for (int i = 0; i < totalBuku; i++) {
+        int tersedia = perpustakaan[i].jumlahTotal - daftarPeminjaman[i].JumlahPinjam;  // Perpustakaan -> perpustakaan, tambah ;
+        printf("%-30s %-15s %-8d %-10d %-10d\n",  // Tambah tanda kutip "
+               perpustakaan[i].Judul,              // Perpustakaan -> perpustakaan
+               perpustakaan[i].Kode,               // Perpustakaan -> perpustakaan
+               perpustakaan[i].jumlahTotal,        // Perpustakaan -> perpustakaan
+               daftarPeminjaman[i].JumlahPinjam,     // daftarPeminjaman -> perpustakaan
+               tersedia);
+        }
+        printf("===============================================================\n");
+
 }
 
 void menu();
