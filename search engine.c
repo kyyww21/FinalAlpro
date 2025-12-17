f95286d60937ff27164370a0b06b20b7ea3fbd18
Papengs <raphaelkingstar@gmail.com>
2025-12-17 23:41:39 +0700

    search engine

--- search engine.c
+++ search engine.c
@@ -8,28 +8,30 @@ struct Buku
     int JumlahTotal;
     int JumlahDipinjam;
 };
-    struct Buku Perpustakaan[100];
-        int totalBuku = 0;
-        void inisialisasidata() {
+    struct Buku perpustakaan[100];
+    int totalBuku = 0;
+
+        void inisialisasiData() {
             strcpy(perpustakaan[0].Judul, "PAI");
             strcpy(perpustakaan[0].Kode, "10");
             perpustakaan[0].JumlahTotal = 365;
-            perpustakaan[0].jumlahDipinjam = 250;
+            perpustakaan[0].JumlahDipinjam = 250;
 
-            strcpy(perpustakaan[0].Judul, "FISIKA");
-            strcpy(perpustakaan[0].Kode, "20");
+            strcpy(perpustakaan[1].Judul, "FISIKA");
+            strcpy(perpustakaan[1].Kode, "20");
             perpustakaan[1].JumlahTotal = 365;
             perpustakaan[1].JumlahDipinjam = 100;
         totalBuku = 2;
+
         }
         void caribuku() {
         char kode[20]; 
         int ditemukan = 0;
-            printf("\n--- PENCARIAN BUKU --\n")'
+            printf("\n--- PENCARIAN BUKU --\n");
             printf("Masukan kode buku : ");
             scanf(" %[^\n]", kode);
-                for (int i = 0; i < totalbuku; i++) {
-                if (strcmp(perpustakaan[i].kode, kode) == 0) {
+                for (int i = 0; i < totalBuku; i++) {
+                if (strcmp(perpustakaan[i].Kode, kode) == 0) {
                 printf("\n=============================\n");
                 printf("   INFORMASI BUKU\n");
                 printf("=============================\n");
@@ -50,12 +52,12 @@ struct Buku
     }
                 int main() {
                     char pilihan;
-                    inisialisasidata();
+                    inisialisasiData();
                     printf("=====================================\n");
                     printf("  SEARCH ENGINE PERPUSTAKAAN\n");
                     printf("=====================================\n");
                 do {
-                    cariBuku();
+                caribuku();
                 printf("\nCari lagi? (y/n) ; ");
                 scanf(" %c", &pilihan);
             } while (pilihan == 'y' || pilihan == 'Y');
@@ -63,4 +65,5 @@ struct Buku
         return 0;
     }
     
+
 
